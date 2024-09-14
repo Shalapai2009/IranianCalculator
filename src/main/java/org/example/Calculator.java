8a4c6ec7d5fabf83a75cf367c3e6a8e735cf4a10
Shalapai2009 <stanislavyakunin76@gmail.com>
2024-09-14 15:58:52 +0300

    calc

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,10 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class Calculator
 {
     public static float num1;
@@ -13,45 +10,23 @@ public class Calculator
     public static String arithmeticOperation;
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите арифметическую операцию");
-        arithmeticOperation = scanner.nextLine();
 
-        System.out.println("Введите 1-ое число");
-        num1 = scanner.nextFloat();
 
-        System.out.println("Введите 2-ое число");
-        num2 = scanner.nextFloat();
 
-        switch (arithmeticOperation){
-            case "+":
-                System.out.println(Sum(num1,num2));
-            case "-":
-                System.out.println(Dif(num1,num2));
-            case "*":
-                System.out.println(Prod(num1,num2));
-            case "/":
-                System.out.println(Div(num1,num2));
 
-        }
-
-        scanner.close();
     }
 
-    public static float Sum(float num1, float num2) {
+    public static float sum(float num1, float num2) {
         return num1 + num2;
     }
-    public static float Dif(float num1, float num2) {
+    public static float dif(float num1, float num2) {
         return num1 - num2;
     }
-    public static float Prod(float num1, float num2) {
+    public static float prod(float num1, float num2) {
         return num1 * num2;
     }
-    public static float Div(float num1, float num2) {
+    public static float div(float num1, float num2) {
         return num1 / num2;
-    }
-    public int sum(int a,int b){
-        return a+b;
     }
 
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,8 +7,26 @@ public class CalculatorTest {
     @Test
     void sum() {
     Calculator calculator = new Calculator();
-    float sum = calculator.Sum(5,10);
+    float sum = calculator.sum(5,10);
         Assertions.assertEquals(15,sum);
+    }
+    @Test
+    void dif() {
+        Calculator calculator = new Calculator();
+        float dif = calculator.dif(10,5);
+        Assertions.assertEquals(5,dif);
+    }
+    @Test
+    void prod() {
+        Calculator calculator = new Calculator();
+        float prod = calculator.prod(5,10);
+        Assertions.assertEquals(50,prod);
+    }
+    @Test
+    void div() {
+        Calculator calculator = new Calculator();
+        float div = calculator.div(50,10);
+        Assertions.assertEquals(5,div);
     }
 
 }
