f24ba1bde830bc1caed4b108650ad02cdfdf1c2a
Shalapai2009 <stanislavyakunin76@gmail.com>
2024-09-14 16:13:45 +0300

    calc

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,30 +1,33 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    private Calculator calculator;
+    @BeforeEach
+    void setUp(){
+    calculator = new Calculator();
+    }
+
     @Test
     void sum() {
-    Calculator calculator = new Calculator();
     float sum = calculator.sum(5,10);
         Assertions.assertEquals(15,sum);
     }
     @Test
     void dif() {
-        Calculator calculator = new Calculator();
         float dif = calculator.dif(10,5);
         Assertions.assertEquals(5,dif);
     }
     @Test
     void prod() {
-        Calculator calculator = new Calculator();
         float prod = calculator.prod(5,10);
         Assertions.assertEquals(50,prod);
     }
     @Test
     void div() {
-        Calculator calculator = new Calculator();
         float div = calculator.div(50,10);
         Assertions.assertEquals(5,div);
     }
