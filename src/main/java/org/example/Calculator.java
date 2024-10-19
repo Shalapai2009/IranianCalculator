159a169636c07d9c6c67b6e0f7fc06d6865a46ae
Shalapai2009 <stanislavyakunin76@gmail.com>
2024-10-19 13:25:59 +0300

    calc

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,10 @@ package org.example;
 
 import java.util.Scanner;
 
-
+/**
+ * Hello world!
+ *
+ */
 public class Calculator
 {
     public static float num1;
@@ -10,8 +13,24 @@ public class Calculator
     public static String arithmeticOperation;
     public static void main( String[] args )
     {
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите арифметическую операцию");
+        arithmeticOperation = scanner.nextLine();
+        System.out.println("Введите 1-ое число");
+        num1 = scanner.nextFloat();
+        System.out.println("Введите 2-ое число");
+        num2 = scanner.nextFloat();
+        switch (arithmeticOperation){
+            case "+":
+                System.out.println(sum(num1,num2));
+            case "-":
+                System.out.println(dif(num1,num2));
+            case "*":
+                System.out.println(prod(num1,num2));
+            case "/":
+                System.out.println(div(num1,num2));
+        }
+        scanner.close();
 
 
     }
